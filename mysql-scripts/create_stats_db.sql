5b0f81e06a02bd368296866d58b8df1600bb9732
Alan <darthsausage@gmail.com>
2016-08-08 22:15:12 -0700

    add mysql import scripts

---
+++ mysql-scripts/create_stats_db.sql
@@ -0,0 +1,533 @@
+-- create db for lahman database
+-- 2015 version
+
+CREATE DATABASE  IF NOT EXISTS `stats` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `stats`;
+
+DROP TABLE IF EXISTS `allstarfull`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `allstarfull` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `gameNum` int(11) DEFAULT NULL,
+  `gameID` text,
+  `teamID` text,
+  `lgID` text,
+  `GP` int(11) DEFAULT NULL,
+  `startingPos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `appearances`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appearances` (
+  `yearID` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `playerID` text,
+  `G_all` int(11) DEFAULT NULL,
+  `GS` text,
+  `G_batting` int(11) DEFAULT NULL,
+  `G_defense` int(11) DEFAULT NULL,
+  `G_p` int(11) DEFAULT NULL,
+  `G_c` int(11) DEFAULT NULL,
+  `G_1b` int(11) DEFAULT NULL,
+  `G_2b` int(11) DEFAULT NULL,
+  `G_3b` int(11) DEFAULT NULL,
+  `G_ss` int(11) DEFAULT NULL,
+  `G_lf` int(11) DEFAULT NULL,
+  `G_cf` int(11) DEFAULT NULL,
+  `G_rf` int(11) DEFAULT NULL,
+  `G_of` int(11) DEFAULT NULL,
+  `G_dh` text,
+  `G_ph` text,
+  `G_pr` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `awardsmanagers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `awardsmanagers` (
+  `playerID` text,
+  `awardID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `lgID` text,
+  `tie` text,
+  `notes` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `awardsplayers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `awardsplayers` (
+  `playerID` text,
+  `awardID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `lgID` text,
+  `tie` text,
+  `notes` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `awardssharemanagers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `awardssharemanagers` (
+  `awardID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `lgID` text,
+  `playerID` text,
+  `pointsWon` int(11) DEFAULT NULL,
+  `pointsMax` int(11) DEFAULT NULL,
+  `votesFirst` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `awardsshareplayers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `awardsshareplayers` (
+  `awardID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `lgID` text,
+  `playerID` text,
+  `pointsWon` int(11) DEFAULT NULL,
+  `pointsMax` int(11) DEFAULT NULL,
+  `votesFirst` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `batting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `batting` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `stint` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `G` int(11) DEFAULT NULL,
+  `AB` int(11) DEFAULT NULL,
+  `R` int(11) DEFAULT NULL,
+  `H` int(11) DEFAULT NULL,
+  `2B` int(11) DEFAULT NULL,
+  `3B` int(11) DEFAULT NULL,
+  `HR` int(11) DEFAULT NULL,
+  `RBI` int(11) DEFAULT NULL,
+  `SB` int(11) DEFAULT NULL,
+  `CS` int(11) DEFAULT NULL,
+  `BB` int(11) DEFAULT NULL,
+  `SO` int(11) DEFAULT NULL,
+  `IBB` text,
+  `HBP` text,
+  `SH` text,
+  `SF` text,
+  `GIDP` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `battingpost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `battingpost` (
+  `yearID` int(11) DEFAULT NULL,
+  `round` text,
+  `playerID` text,
+  `teamID` text,
+  `lgID` text,
+  `G` int(11) DEFAULT NULL,
+  `AB` int(11) DEFAULT NULL,
+  `R` int(11) DEFAULT NULL,
+  `H` int(11) DEFAULT NULL,
+  `2B` int(11) DEFAULT NULL,
+  `3B` int(11) DEFAULT NULL,
+  `HR` int(11) DEFAULT NULL,
+  `RBI` int(11) DEFAULT NULL,
+  `SB` int(11) DEFAULT NULL,
+  `CS` text,
+  `BB` int(11) DEFAULT NULL,
+  `SO` int(11) DEFAULT NULL,
+  `IBB` int(11) DEFAULT NULL,
+  `HBP` text,
+  `SH` text,
+  `SF` text,
+  `GIDP` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `collegeplaying`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `collegeplaying` (
+  `playerID` text,
+  `schoolID` text,
+  `yearID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `fielding`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fielding` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `stint` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `POS` text,
+  `G` int(11) DEFAULT NULL,
+  `GS` text,
+  `InnOuts` text,
+  `PO` int(11) DEFAULT NULL,
+  `A` int(11) DEFAULT NULL,
+  `E` int(11) DEFAULT NULL,
+  `DP` int(11) DEFAULT NULL,
+  `PB` text,
+  `WP` text,
+  `SB` text,
+  `CS` text,
+  `ZR` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `fieldingof`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fieldingof` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `stint` int(11) DEFAULT NULL,
+  `Glf` int(11) DEFAULT NULL,
+  `Gcf` int(11) DEFAULT NULL,
+  `Grf` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `fieldingpost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fieldingpost` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `round` text,
+  `POS` text,
+  `G` int(11) DEFAULT NULL,
+  `GS` int(11) DEFAULT NULL,
+  `InnOuts` int(11) DEFAULT NULL,
+  `PO` int(11) DEFAULT NULL,
+  `A` int(11) DEFAULT NULL,
+  `E` int(11) DEFAULT NULL,
+  `DP` int(11) DEFAULT NULL,
+  `TP` int(11) DEFAULT NULL,
+  `PB` text,
+  `SB` text,
+  `CS` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `halloffame`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `halloffame` (
+  `playerID` text,
+  `yearid` int(11) DEFAULT NULL,
+  `votedBy` text,
+  `ballots` int(11) DEFAULT NULL,
+  `needed` int(11) DEFAULT NULL,
+  `votes` int(11) DEFAULT NULL,
+  `inducted` text,
+  `category` text,
+  `needed_note` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `homegames`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `homegames` (
+  `year.key` int(11) DEFAULT NULL,
+  `league.key` text,
+  `team.key` text,
+  `park.key` text,
+  `span.first` text,
+  `span.last` text,
+  `games` int(11) DEFAULT NULL,
+  `openings` int(11) DEFAULT NULL,
+  `attendance` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `managers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `managers` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `inseason` int(11) DEFAULT NULL,
+  `G` int(11) DEFAULT NULL,
+  `W` int(11) DEFAULT NULL,
+  `L` int(11) DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `plyrMgr` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `managershalf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `managershalf` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `inseason` int(11) DEFAULT NULL,
+  `half` int(11) DEFAULT NULL,
+  `G` int(11) DEFAULT NULL,
+  `W` int(11) DEFAULT NULL,
+  `L` int(11) DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `master`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `master` (
+  `playerID` text,
+  `birthYear` int(11) DEFAULT NULL,
+  `birthMonth` int(11) DEFAULT NULL,
+  `birthDay` int(11) DEFAULT NULL,
+  `birthCountry` text,
+  `birthState` text,
+  `birthCity` text,
+  `deathYear` text,
+  `deathMonth` text,
+  `deathDay` text,
+  `deathCountry` text,
+  `deathState` text,
+  `deathCity` text,
+  `nameFirst` text,
+  `nameLast` text,
+  `nameGiven` text,
+  `weight` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `bats` text,
+  `throws` text,
+  `debut` text,
+  `finalGame` text,
+  `retroID` text,
+  `bbrefID` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `parks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parks` (
+  `park.key` text,
+  `park.name` text,
+  `park.alias` text,
+  `city` text,
+  `state` text,
+  `country` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `pitching`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pitching` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `stint` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `W` int(11) DEFAULT NULL,
+  `L` int(11) DEFAULT NULL,
+  `G` int(11) DEFAULT NULL,
+  `GS` int(11) DEFAULT NULL,
+  `CG` int(11) DEFAULT NULL,
+  `SHO` int(11) DEFAULT NULL,
+  `SV` int(11) DEFAULT NULL,
+  `IPouts` int(11) DEFAULT NULL,
+  `H` int(11) DEFAULT NULL,
+  `ER` int(11) DEFAULT NULL,
+  `HR` int(11) DEFAULT NULL,
+  `BB` int(11) DEFAULT NULL,
+  `SO` int(11) DEFAULT NULL,
+  `BAOpp` text,
+  `ERA` double DEFAULT NULL,
+  `IBB` text,
+  `WP` text,
+  `HBP` text,
+  `BK` int(11) DEFAULT NULL,
+  `BFP` text,
+  `GF` text,
+  `R` int(11) DEFAULT NULL,
+  `SH` text,
+  `SF` text,
+  `GIDP` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `pitchingpost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pitchingpost` (
+  `playerID` text,
+  `yearID` int(11) DEFAULT NULL,
+  `round` text,
+  `teamID` text,
+  `lgID` text,
+  `W` int(11) DEFAULT NULL,
+  `L` int(11) DEFAULT NULL,
+  `G` int(11) DEFAULT NULL,
+  `GS` int(11) DEFAULT NULL,
+  `CG` int(11) DEFAULT NULL,
+  `SHO` int(11) DEFAULT NULL,
+  `SV` int(11) DEFAULT NULL,
+  `IPouts` int(11) DEFAULT NULL,
+  `H` int(11) DEFAULT NULL,
+  `ER` int(11) DEFAULT NULL,
+  `HR` int(11) DEFAULT NULL,
+  `BB` int(11) DEFAULT NULL,
+  `SO` int(11) DEFAULT NULL,
+  `BAOpp` text,
+  `ERA` double DEFAULT NULL,
+  `IBB` text,
+  `WP` text,
+  `HBP` text,
+  `BK` text,
+  `BFP` text,
+  `GF` int(11) DEFAULT NULL,
+  `R` int(11) DEFAULT NULL,
+  `SH` text,
+  `SF` text,
+  `GIDP` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `salaries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `salaries` (
+  `yearID` int(11) DEFAULT NULL,
+  `teamID` text,
+  `lgID` text,
+  `playerID` text,
+  `salary` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `schools`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schools` (
+  `schoolID` text,
+  `name_full` text,
+  `city` text,
+  `state` text,
+  `country` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `seriespost`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seriespost` (
+  `yearID` int(11) DEFAULT NULL,
+  `round` text,
+  `teamIDwinner` text,
+  `lgIDwinner` text,
+  `teamIDloser` text,
+  `lgIDloser` text,
+  `wins` int(11) DEFAULT NULL,
+  `losses` int(11) DEFAULT NULL,
+  `ties` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teams` (
+  `yearID` int(11) DEFAULT NULL,
+  `lgID` text,
+  `teamID` text,
+  `franchID` text,
+  `divID` text,
+  `Rank` int(11) DEFAULT NULL,
+  `G` int(11) DEFAULT NULL,
+  `Ghome` text,
+  `W` int(11) DEFAULT NULL,
+  `L` int(11) DEFAULT NULL,
+  `DivWin` text,
+  `WCWin` text,
+  `LgWin` text,
+  `WSWin` text,
+  `R` int(11) DEFAULT NULL,
+  `AB` int(11) DEFAULT NULL,
+  `H` int(11) DEFAULT NULL,
+  `2B` int(11) DEFAULT NULL,
+  `3B` int(11) DEFAULT NULL,
+  `HR` int(11) DEFAULT NULL,
+  `BB` int(11) DEFAULT NULL,
+  `SO` int(11) DEFAULT NULL,
+  `SB` int(11) DEFAULT NULL,
+  `CS` text,
+  `HBP` text,
+  `SF` text,
+  `RA` int(11) DEFAULT NULL,
+  `ER` int(11) DEFAULT NULL,
+  `ERA` double DEFAULT NULL,
+  `CG` int(11) DEFAULT NULL,
+  `SHO` int(11) DEFAULT NULL,
+  `SV` int(11) DEFAULT NULL,
+  `IPouts` int(11) DEFAULT NULL,
+  `HA` int(11) DEFAULT NULL,
+  `HRA` int(11) DEFAULT NULL,
+  `BBA` int(11) DEFAULT NULL,
+  `SOA` int(11) DEFAULT NULL,
+  `E` int(11) DEFAULT NULL,
+  `DP` text,
+  `FP` double DEFAULT NULL,
+  `name` text,
+  `park` text,
+  `attendance` text,
+  `BPF` int(11) DEFAULT NULL,
+  `PPF` int(11) DEFAULT NULL,
+  `teamIDBR` text,
+  `teamIDlahman45` text,
+  `teamIDretro` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `teamsfranchises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teamsfranchises` (
+  `franchID` text,
+  `franchName` text,
+  `active` text,
+  `NAassoc` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `teamshalf`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teamshalf` (
+  `yearID` int(11) DEFAULT NULL,
+  `lgID` text,
+  `teamID` text,
+  `Half` int(11) DEFAULT NULL,
+  `divID` text,
+  `DivWin` text,
+  `Rank` int(11) DEFAULT NULL,
+  `G` int(11) DEFAULT NULL,
+  `W` int(11) DEFAULT NULL,
+  `L` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+CREATE INDEX MASTER_PLAYER ON master ( playerID(10) );
+CREATE INDEX APP_TEAM ON appearances ( teamID(10) );
+CREATE INDEX APP_PLAYER ON appearances ( playerID(10) );
